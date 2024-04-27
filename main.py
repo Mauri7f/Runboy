5ccc3cd5983e27bb8cd079da56da1860c4cd14f3
Mauri7f <165766343+Mauri7f@users.noreply.github.com>
2024-04-27 08:40:02 -1000

    Put player and enemies into classes. Added background music.

--- main.py
+++ main.py
@@ -1,6 +1,83 @@
 import pygame
 from sys import exit
-from random import randint
+from random import randint, choice
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+        player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+        self.player_walk = [player_walk_1, player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('graphics/player/player_jump.png').convert_alpha()
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom=(150, 320))
+        self.gravity = 0
+
+        self.jump_sound = pygame.mixer.Sound('audio/jump.wav')
+        self.jump_sound.set_volume(0.4)
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 320:
+            self.gravity = -20
+            self.jump_sound.play()
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 320:
+            self.rect.bottom = 320
+
+    def animation_state(self):
+        if self.rect.bottom < 320:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.2
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+
+        if type == 'beast':
+            beast = pygame.image.load('graphics/beast/beast1.png')
+            self.frames = [beast]
+            y_pos = 210
+        else:
+            fiend_1 = pygame.image.load('graphics/fiend/f_1.png').convert_alpha()
+            fiend_2 = pygame.image.load('graphics/fiend/f_2.png').convert_alpha()
+            self.frames = [fiend_1, fiend_2]
+            y_pos = 320
+
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
+
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames):
+            self.animation_index = 0
+            self.image = self.frames[int(self.animation_index)]
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= 6
+        self.destroy()
+
+    def destroy(self):
+        if self.rect.x <= -100:
+            self.kill()
 
 
 def display_score():
@@ -35,6 +112,13 @@ def collisions(player, obstacles):
     return True
 
 
+def collision_sprite():
+    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
+        obstacle_group.empty()
+        return False
+    else: return True
+
+
 def player_animation():
     global player_surf, player_index
 
@@ -55,6 +139,15 @@ test_font = pygame.font.Font(None, 50)
 game_active = True
 start_time = 0
 score = 0
+bg_music = pygame.mixer.Sound('audio/music.mp3')
+bg_music.play(loops=-1)
+bg_music.set_volume(0.5)
+
+# Groups
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+obstacle_group = pygame.sprite.Group()
 
 back_surface = pygame.image.load('background.png').convert()
 ground = pygame.image.load('ground.png').convert()
@@ -94,8 +187,8 @@ game_message_rect = game_message.get_rect(center=(400, 370))
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 1400)
 
-scorpion_animation_timer = pygame.USEREVENT + 2
-pygame.time.set_timer(scorpion_animation_timer, 500)
+fiend_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(fiend_animation_timer, 500)
 
 while True:
     for event in pygame.event.get():
@@ -115,11 +208,9 @@ while True:
 
         if game_active:
             if event.type == obstacle_timer and game_active:
-                if randint(0,2):
-                    obstacle_rect_list.append(fiend_surf.get_rect(bottomright=(randint(900, 1100), 320)))
-                else:
-                    obstacle_rect_list.append(beast_surf.get_rect(bottomright=(randint(900, 1100), 200)))
-            if event.type == scorpion_animation_timer:
+                obstacle_group.add(Obstacle(choice(['beast', 'fiend', 'fiend', 'fiend'])))
+
+            if event.type == fiend_animation_timer:
                 if fiend_frame_index == 0:
                     fiend_frame_index = 1
                 else: fiend_frame_index = 0
@@ -131,19 +222,13 @@ while True:
         screen.blit(ground, (400, 320))
         score = display_score()
 
-        # Player
-        player_gravity += 1
-        player_rect.y += player_gravity
-        if player_rect.bottom >= 320:
-            player_rect.bottom = 320
-        player_animation()
-        screen.blit(player_surf, player_rect)
+        player.draw(screen)
+        player.update()
 
-        # Obstacle movement
-        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+        obstacle_group.draw(screen)
+        obstacle_group.update()
 
-        # collision
-        game_active = collisions(player_rect, obstacle_rect_list)
+        game_active = collision_sprite()
 
     else:
         screen.fill((47, 7, 99))
